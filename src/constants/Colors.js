a6bb7bbce39d414017565f6095bc735cdc8e459b
parag.mer <paragmer2001@gmail.com>
2023-07-31 16:00:08 +0530

    31 jul PM

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -2,9 +2,13 @@ const COLORS = {
   WHITE: '#ffffff',
   BLACK: '#000000',
   GRAY100: '#e4e5e4',
+  GRAY200: '#cccccc',
   GRAY300: '#7d7f8b',
   GRAY500: '#6b6b6b',
   RED: '#D84E55',
+  SUCCESS : '#38b87c',
+  WARNING : '#ebaf3b',
+  DANGER : '#f04747',
 };
 
 export default COLORS;
