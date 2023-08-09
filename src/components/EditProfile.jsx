1b507e2863ca830cf9e3eb6a43298fd6f0b48880
parag.mer <paragmer2001@gmail.com>
2023-08-09 10:28:39 +0530

    new file

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,8 +1,18 @@
-import { Text } from "react-native";
+import { Text, TextInput, View } from "react-native";
 
 export default function EditProfile()
 {
     return(
-        <Text>Edit Profile</Text>
+        <View>
+          <Text>Name</Text>
+          <TextInput placeholder="Enter Name" type/>  
+          <Text>Email</Text>
+          <TextInput placeholder="Enter Name" keyboardType='email-address'/>
+          <Text>Age</Text>
+          <TextInput placeholder="Enter Name" keyboardType='number-pad'/>
+          <Text>Contact No.</Text>
+          <TextInput placeholder="Enter Name" keyboardType='number-pad'/>
+          
+        </View>
     );
 }
